d62f3b7213a95780fe8f6b654c974062b582e1e5
Warro <olli.warro@gmail.com>
2019-11-20 11:23:06 +0200

    Added browserify default options just in case

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ module.exports = config => {
   const transform = [...browserify.defaultOptions.browserifyOptions.transform]
   if (config.env.smokify) transform.push(smokify(config.env.smokify))
   const options = {
+    ...browserify.defaultOptions,
     browserifyOptions: {
+      ...browserify.defaultOptions.browserifyOptions,
       transform
     }
   }
